360ecf1c150cc7fb6f3dfacad2e05bea27e6e028
YuriPetukhov <petuchowjuri@yahoo.de>
2024-09-26 16:55:35 +0300

    lecture 3 Add close button translation in the basket

--- src/components/modal-layout/index.js
+++ src/components/modal-layout/index.js
@@ -1,10 +1,12 @@
 import { memo, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { cn as bem } from '@bem-react/classname';
+import { useTranslation } from '../../use-translation';
 import './style.css';
 
 function ModalLayout(props) {
   const cn = bem('ModalLayout');
+  const translate = useTranslation();
 
   // Корректировка центра, если модалка больше окна браузера.
   const layout = useRef();
@@ -30,7 +32,7 @@ function ModalLayout(props) {
         <div className={cn('head')}>
           <h1 className={cn('title')}>{props.title}</h1>
           <button className={cn('close')} onClick={props.onClose}>
-            Закрыть
+          {translate('basket.close')}
           </button>
         </div>
         <div className={cn('content')}>{props.children}</div>
